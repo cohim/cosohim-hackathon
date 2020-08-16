72e76e88d5027c48aae920c71ca82fcd11ccfcab
kimtaegwan <ab154@naver.com>
2020-08-16 14:42:15 +0900

    Add react-bootstrap

--- src/components/MainComponent/DropdownInput/index.js
+++ src/components/MainComponent/DropdownInput/index.js
@@ -1,49 +1,18 @@
 import React from 'react'
-
-import arrow from "../../../assets/images/arrow.png";
-import mag from "../../../assets/images/mag.png";
+import { Dropdown } from 'react-bootstrap'
 
 const DropdownInput = () => {
-
-    const folder = () => {
-
-        var target = document.querySelector('#data');
-        var bicon = document.querySelector('#btni');
-        var magni = document.querySelector('#bmag');
-
-      if(this.value==='내용접기'){
-        target.style.display = 'block';
-        magni.style.display='block';
-        bicon.style.transform = 'rotate(180deg)';
-        this.value = '내용펼치기';
-      }
-      else{
-        target.style.display ='none';
-        magni.style.display='none';
-        bicon.style.transform = 'rotate(360deg)';
-        this.value = '내용접기';
-      }
-
-    }
-
     return (
-        <div className="search_dropdown_bar_container">
-          <div>
-            <section>
-              <div className= "searchbutton">
-                <input className="search" type="text" placeholder="맞춤형 상품검색 " />
-                <button type="button" className= "btn" value="내용접기" onClick={folder}>
-                <img id= "btni" src={arrow}alt="" /></button>
-                <button id= "bmag" type="button" value="확대">
-                    <img className= "mag" src={mag} alt="" />
-
-                    </button>
-              </div>
-              <br /><br />
-              <p id="data"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </section>
-          </div>
-        </div>
+        <Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+                Dropdown Button
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+            </Dropdown.Menu>
+        </Dropdown>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.css';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
     <App />,
