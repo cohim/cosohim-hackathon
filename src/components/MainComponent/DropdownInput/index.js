d73e7a4c9cc01619a0e47071790a906151b730d4
mjy18 <1110ymj@sookmyung.ac.kr>
2020-08-16 19:57:59 +0900

    dropbox

--- src/components/MainComponent/DropdownInput/index.js
+++ src/components/MainComponent/DropdownInput/index.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import { Dropdown } from 'react-bootstrap'
+import mag from "../../../assets/images/mag.png"
 
 const DropdownInput = () => {
     return (
+        <div className = "dropbar">
         <Dropdown>
             <Dropdown.Toggle variant="success" id="dropdown-basic">
-                Dropdown Button
+                개인별 맞춤 상품 검색 
             </Dropdown.Toggle>
             <Dropdown.Menu>
-                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                {/* <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
                 <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item> */}
             </Dropdown.Menu>
+              <img src={mag} alt="search_img" width="100%" height="100%" className="mag" />
         </Dropdown>
+        </div>
+
     )
 }
 
