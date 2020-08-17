7eb0af1dc65d046d91d768f98f8af836b41385a0
kimtaegwan <ab154@naver.com>
2020-08-17 21:04:11 +0900

    Edit banner img or Add axios

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -2,11 +2,13 @@ import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
 import { Container } from 'react-bootstrap'
+import MainBanner from '../components/MainComponent/MainBanner'
 
 const Layout = ({children}) => {
     return (
         <div className="layout">
             <Header />
+                <MainBanner />
                 <Container>
                     {children}
                 </Container>
