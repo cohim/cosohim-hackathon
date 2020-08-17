61225038fdc01992ca25420b5d1fe525ebf937f8
sungeun <9901bae0906@gmail.com>
2020-08-17 22:36:52 +0900

    recommend

--- src/components/MainComponent/DropdownInput/index.js
+++ src/components/MainComponent/DropdownInput/index.js
@@ -1,25 +1,6 @@
 import React from 'react'
 import { Dropdown } from 'react-bootstrap'
 import mag from "../../../assets/images/mag.png"
-<<<<<<< HEAD
-
-const DropdownInput = () => {
-    return (
-        <div className = "dropbar">
-        <Dropdown>
-            <Dropdown.Toggle variant="success" id="dropdown-basic">
-                개인별 맞춤 상품 검색 
-            </Dropdown.Toggle>
-            <Dropdown.Menu>
-                {/* <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item> */}
-            </Dropdown.Menu>
-              <img src={mag} alt="search_img" width="100%" height="100%" className="mag" />
-        </Dropdown>
-        </div>
-
-=======
 import Button from 'react-bootstrap/Button'
 
 
@@ -44,7 +25,6 @@ const DropdownInput = () => {
                 </Button>{' '}
             </div>
         </div>
->>>>>>> 5536808e6a0809de757047e2ca45bafe695b3b63
     )
 }
 
