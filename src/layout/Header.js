9becaa9df4d89edd24c18144d501a94f96d0e89b
kaeun <kadoridang@gmail.com>
2020-08-18 01:54:49 +0900

    chatbot before modal

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -4,7 +4,7 @@ import { Navbar, Nav, NavDropdown, Form} from 'react-bootstrap'
 const Header = () => {
     return (
         <Navbar bg="light" expand="lg">
-            <Navbar.Brand href="#home" img src="/2019/lib/image/img-gov-logo.png" alt="코힘">로고</Navbar.Brand>
+            <Navbar.Brand href="#home" img src="" alt="코힘">로고</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
