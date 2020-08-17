99cf7d683df9bb64654a1a6863360912f8c68de8
mjy18 <1110ymj@sookmyung.ac.kr>
2020-08-18 05:24:06 +0900

    qna complete

--- src/components/QnaformComponent/index.js
+++ src/components/QnaformComponent/index.js
@@ -1,25 +1,32 @@
 import React from 'react'
 import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
 const QnaformComponent = () => {
     return (
         <div className="qnaform">
+            <header>
+                <h4>위의 양식에 맞춰 문의 사항을 남겨주세요. <br/><br/>문의하신 내용은 코힘 운영자에게 전달됩니다. </h4><hr/><br/><br/></header>
             <Form>
                 <Form.Group controlId="exampleForm.ControlInput1">
                     <Form.Label>답변을 받을 이메일 주소</Form.Label>
                     <Form.Control type="email" placeholder="name@example.com" />
+                    <br/>
                 </Form.Group>
                 <Form.Group controlId="exampleForm.ControlSelect1">
                 </Form.Group>
                 <Form.Group controlId="exampleForm.ControlTextarea1">
                     <Form.Label>문의사항</Form.Label>
                     <Form.Control as="textarea" rows="10" />
+                    <br/>
                 </Form.Group>
             </Form>
-            <footer>
-            <hr/><h4>위의 양식에 맞춰 문의 사항을 남겨주세요. <br/><br/>문의하신 내용은 코힘 운영자에게 전달됩니다. </h4><br/><br/></footer>
+            <div className="qnaformbtn">
+                <Button as="input" type="submit" value="Submit" />{' '}
+                <Button as="input" type="reset" value="Reset" />
+            </div>
         </div>
-    )
+        )
 }
 
 export default QnaformComponent
