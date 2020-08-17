c9c6f0b719fdf2dc9c08ff8347931020d0b99a6e
mjy18 <1110ymj@sookmyung.ac.kr>
2020-08-17 15:57:47 +0900

    add search.png

--- src/components/MainComponent/DropdownInput/index.js
+++ src/components/MainComponent/DropdownInput/index.js
@@ -1,33 +1,23 @@
 import React from 'react'
 import { Dropdown } from 'react-bootstrap'
-import mag from "../../../assets/images/mag.png"
-<<<<<<< HEAD
-
-const DropdownInput = () => {
-    return (
-        <div className = "dropbar">
-        <Dropdown>
-            <Dropdown.Toggle variant="success" id="dropdown-basic">
-                개인별 맞춤 상품 검색 
-            </Dropdown.Toggle>
-            <Dropdown.Menu>
-                {/* <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item> */}
-            </Dropdown.Menu>
-              <img src={mag} alt="search_img" width="100%" height="100%" className="mag" />
-        </Dropdown>
-        </div>
-
-=======
+import search from "../../../assets/images/search.png"
 import Button from 'react-bootstrap/Button'
 
+// function searchbtn(){
+//     var btn = document.querySelector('.magbtn');
+//     if(onclick===true){
+//         btn.style.display = 'block';
+//     }
+//     else{
+//         btn.style.display = 'none';
+//     }
+// }
 
 const DropdownInput = () => {
     return (
         <div class = "searchbtn">
             <div className="dropbar">
-                <Dropdown>
+                <Dropdown onclik= "searchbtn()">
                     <Dropdown.Toggle variant="success" id="dropdown-basic">
                          개인별 맞춤 상품 검색
                     </Dropdown.Toggle>
@@ -39,12 +29,11 @@ const DropdownInput = () => {
                 </Dropdown>
             </div>
             <div className= "search_mag">
-                <Button id = "magbtn" variant="link">
-                <img src={mag} className = 'mag' alt="mag_img"/>
+                <Button id = "search" variant="link">
+                <img src={search} className = 'search' alt="search_img"/>
                 </Button>{' '}
             </div>
         </div>
->>>>>>> 5536808e6a0809de757047e2ca45bafe695b3b63
     )
 }
 
