2fb8a04526079855444770d27491d570ffe60e88
mjy18 <1110ymj@sookmyung.ac.kr>
2020-08-18 01:20:58 +0900

    qna

--- src/components/NoticeComponent/index.js
+++ src/components/NoticeComponent/index.js
@@ -7,20 +7,24 @@ import TableData from '../Items/Table/TableData';
 const NoticeComponent = () => {
     return (
         <div className="notice">
+            <aside className = "aside">
+                <ul>
+                    <li><h4>공지사항</h4></li>
+                    <li><Link to='/notice'><p>공지사항</p></Link></li>
+                    <li><Link to="/news"><p>보도자료</p></Link></li>
+                </ul>
+            </aside>
             <seaction className="seaction">
-                <Table
-                tableHeaderName="공지사항"
-                >
+                <Table tableHeaderName="공지사항">
                     <TableData number="1" noticeName="신용보증제한기업소개" noticeLink="#" />
+                    <TableData number="2" noticeName="신용보증제한기업소개" noticeLink="#" />
+                    <TableData number="3" noticeName="신용보증제한기업소개" noticeLink="#" />
+                    <TableData number="4" noticeName="신용보증제한기업소개" noticeLink="#" />
+                    <TableData number="5" noticeName="신용보증제한기업소개" noticeLink="#" />
+                    <TableData number="6" noticeName="신용보증제한기업소개" noticeLink="#" />
+                    <TableData number="7" noticeName="신용보증제한기업소개" noticeLink="#" />
                 </Table>
             </seaction>
-            <aside className = "aside">
-                    <ul>
-                        <li><h4>공지사항</h4></li>
-                        <li><Link to='/notice'><p>공지사항</p></Link></li>
-                        <li><Link to="/news"><p>보도자료</p></Link></li>
-                    </ul>
-            </aside>
         </div>
     )
 }
