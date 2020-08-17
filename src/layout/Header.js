4ed0bc88ec8ceb9f8041c0f9c8ec584522d6d451
kimtaegwan <ab154@naver.com>
2020-08-18 04:44:06 +0900

    Edit header link

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -4,7 +4,7 @@ import { Navbar, Nav, NavDropdown, Form} from 'react-bootstrap'
 const Header = () => {
     return (
         <Navbar bg="light" expand="lg" style={{margin:0, fontSize:"1.5rem"}}>
-            <Navbar.Brand href="#home" img src="/2019/lib/image/img-gov-logo.png" alt="코힘">로고</Navbar.Brand>
+            <Navbar.Brand href="/" img src="/2019/lib/image/img-gov-logo.png" alt="코힘">로고</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
@@ -16,16 +16,16 @@ const Header = () => {
                     <NavDropdown.Item href="/foundation#foundation">재단 소개</NavDropdown.Item>
                     <NavDropdown.Item href="/foundation#ceo">CEO 소개</NavDropdown.Item>
                 </NavDropdown>
-                <Nav.Link href="#link">보증절차</Nav.Link>
-                <Nav.Link href="#link">상품소개</Nav.Link>
+                <Nav.Link href="process">보증절차</Nav.Link>
+                <Nav.Link href="productsList">상품소개</Nav.Link>
                 <NavDropdown title="공지사항" id="basic-nav-dropdown">
-                    <NavDropdown.Item href="#action/3.1">공지사항</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2">보도자료</NavDropdown.Item>
+                    <NavDropdown.Item href="notice">공지사항</NavDropdown.Item>
+                    <NavDropdown.Item href="news">보도자료</NavDropdown.Item>
                 </NavDropdown>
                 <NavDropdown title="고객센터" id="basic-nav-dropdown">
-                    <NavDropdown.Item href="#action/3.1" >자주 하는 질문</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2" >문의 작성</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.3" >문의 답변</NavDropdown.Item>
+                    <NavDropdown.Item href="qna" >자주 하는 질문</NavDropdown.Item>
+                    <NavDropdown.Item href="qnaform" >문의 작성</NavDropdown.Item>
+                    <NavDropdown.Item href="qna" >문의 답변</NavDropdown.Item>
                 </NavDropdown>
                 </Nav>
                 </Form>
