50408227c678a9e4b1812d8663c5264c237c2476
mjy18 <1110ymj@sookmyung.ac.kr>
2020-08-18 03:38:55 +0900

    qna

---
+++ src/containers/QnaformContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import QnaformComponent from '../components/QnaformComponent'
+
+const QnaformContainer = () => {
+    return (
+        <div>
+            <QnaformComponent/>
+        </div>
+    )
+}
+
+export default QnaformContainer
