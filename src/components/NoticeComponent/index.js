4d05f023a5e02a8e2485c8e5ee39660216d3d77a
mjy18 <1110ymj@sookmyung.ac.kr>
2020-08-17 18:26:31 +0900

    notice css

--- src/components/NoticeComponent/index.js
+++ src/components/NoticeComponent/index.js
@@ -1,9 +1,31 @@
 import React from 'react'
 
+
 const NoticeComponent = () => {
     return (
-        <div>
-            공지사항
+        <div className="notice">
+            <seaction>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>신용보증제한기업</th>
+                            <th>신용보증기업</th>
+                            <th>신용보증기업</th>
+                            <th>신용보증기업</th>
+                            <th>신용보증기업</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                    </tbody>
+                </table>
+            </seaction>
+            <aside className = "aside">
+                    <ul>
+                        <li><a href=""><p>공지사항</p></a></li>
+                        <li><a href=""><p>보도자료</p></a></li>
+                    </ul>
+            </aside>
         </div>
     )
 }
