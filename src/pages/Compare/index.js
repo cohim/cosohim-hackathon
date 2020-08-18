b6c43c3e3a92f8caad0b221fa80139fbe09e18fa
kimtaegwan <ab154@naver.com>
2020-08-18 14:30:09 +0900

    Edit style

---
+++ src/pages/Compare/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ComapreContainer from '../../containers/ComapreContainer'
+
+const Comapre = () => {
+    return (
+        <ComapreContainer />
+    )
+}
+
+export default Comapre
